6bb1fd6e3aefcd579aac16337cd9c7622f3195fa
RahulSharma2004 <rahul702.be22@chitkara.edu.in>
2024-11-11 15:00:02 +0530

    made validate token for doctor

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const doctorDetailControllers = require("../controllers/doctorDetailControllers");
-const jwtAuthMiddleware = require("../middlewares/jwtMiddleware");
+const {validateJwtToken} = require("../middlewares/jwtMiddleware");
 
 router.post("/register", doctorDetailControllers.registerDoctor);
-// router.get("/", doctorDetailControllers.getAllDoctors)
-// router.get("/email/:email", doctorDetailControllers.g);
+
+
+router.get("/alldoctors", validateJwtToken, doctorDetailControllers.getAllDoctors);
+
 module.exports = router;
